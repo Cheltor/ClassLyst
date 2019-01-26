a85ce61ae09323f1b20db1e564479522552f58da
Cheltor <rchelton@umd.edu>
2019-01-25 21:59:26 -0500

    uniiversity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_013125) do
+ActiveRecord::Schema.define(version: 2019_01_26_021107) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2019_01_26_013125) do
     t.index ["confirmation_token"], name: "index_businesses_on_confirmation_token", unique: true
     t.index ["email"], name: "index_businesses_on_email", unique: true
     t.index ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
