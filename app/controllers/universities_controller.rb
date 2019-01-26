41d4312cce0b482dd2b0fa341a3e9dfdb8e14b87
Cheltor <rchelton@umd.edu>
2019-01-26 15:23:44 -0500

    courses and email fix

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,5 +1,6 @@
 class UniversitiesController < ApplicationController
   before_action :set_university, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   before_action :admin_user
 
   # GET /universities
